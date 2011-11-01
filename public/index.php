9e7adc59b691d16ad616405229a92a906f9295f3
Dan Horrigan <dan@dhorrigan.com>
2011-11-01 10:17:17 -0700

    Request->dispatch now is expected by index.php to return a valid Response object by default.

--- public/index.php
+++ public/index.php
@@ -4,4 +4,4 @@ include dirname(__DIR__).'/apps/bootstrap.php';
 Fuel\Foundation\Kernel::load_app('main');
 
 $app = new Main\Application('dev');
-echo $app->serve(Fuel\Http\Request::make());
+echo $app->serve(Fuel\Http\Request::make())->send();

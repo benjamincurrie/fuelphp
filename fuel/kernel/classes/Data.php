0f2158fd72c21afa7948aba1166a830173c9d874
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2012-02-20 02:17:19 +0100

    Added dots to array helper function and started Data class implementation.

--- fuel/kernel/classes/Data.php
+++ fuel/kernel/classes/Data.php
@@ -7,12 +7,61 @@ abstract class Data
 	/**
 	 * @var  array  keeps the data
 	 */
-	protected $data = array();
+	protected $_data = array();
 
 	/**
 	 * @var  array  descendants of this class
 	 */
-	protected $children = array();
+	protected $_children = array();
 
+	/**
+	 * Gets a dot-notated key from data, with a default value if it does not exist.
+	 *
+	 * @param   mixed   $key      The dot-notated key or array of keys
+	 * @param   string  $default  The default value
+	 * @return  mixed
+	 */
+	public function get($key, $default = null)
+	{
+		// Return full data array on null input
+		if (is_null($key))
+		{
+			return $this->_data;
+		}
 
+		// Fetch multiple keys
+		if (is_array($key))
+		{
+			$return = array();
+			foreach ($key as $k)
+			{
+				$return[$k] = $this->get($k, $default);
+			}
+			return $return;
+		}
+
+		return dots_to_array($key, $this->_data, $return) ? $return : $default;
+	}
+
+	/**
+	 * Set a data item (dot-notated) to the value.
+	 *
+	 * @param   mixed   $key    The dot-notated key to set or array of keys
+	 * @param   mixed   $value  The value
+	 * @return  Data    for method chaining
+	 */
+	public function set($key, $value = null)
+	{
+		if (is_null($value) and is_array($key))
+		{
+			foreach ($key as $k => $v)
+			{
+				$this->set($k, $v);
+			}
+			return $this;
+		}
+
+		dots_to_array($key, $this->_data, $value);
+		return $this;
+	}
 }
--- fuel/kernel/helpers.php
+++ fuel/kernel/helpers.php
@@ -29,3 +29,52 @@ function _forge()
 {
 	return call_user_func_array(array(_loader(), 'forge'), func_get_args());
 }
+
+/**
+ * Translates a dot notated key to a value from the given data.
+ * It returns success of the operation, actual value found is returned as a reference
+ *
+ * @param   string              $key
+ * @param   array|\ArrayAccess  $data
+ * @param   mixed               $return
+ * @param   bool                $setting
+ * @return  bool
+ */
+function dots_to_array($key, &$data, &$return)
+{
+	// When the return is provided this is a set operation
+	$setting  = $return;
+	// Make the return var the data now
+	$return   = $data;
+
+	// Explode the key and start iterating
+	$keys = explode('.', $key);
+	while (count($keys) > 1)
+	{
+		$key = array_shift($keys);
+		if ( ! isset($return[$key])
+			or ( ! is_array($return[$key]) and ! $return[$key] instanceof \ArrayAccess))
+		{
+			if (is_null($setting))
+			{
+				// Value not found, return failure
+				return false;
+			}
+			else
+			{
+				// Create new subarray or overwrite non array
+				$return[$key] = array();
+			}
+		}
+		$return =& $return[$key];
+	}
+
+	// Set when this is a set operation
+	if ( ! is_null($setting))
+	{
+		$return = $setting;
+	}
+
+	// return success
+	return true;
+}

e48e850d8a20385b0e6c9b44360be392814c4048
Jelmer Schreuder <j.schreuder@mijnpraktijk.com>
2012-02-24 16:31:32 +0100

    Added wildcard route for further routes testing

--- fuel/app/application.php
+++ fuel/app/application.php
@@ -8,5 +8,7 @@ class App extends Application\Base
 	public function router()
 	{
 		$this->add_route('/', 'welcome');
+
+		$this->add_route('(.*)', 'welcome/catchall/$1');
 	}
 }
--- fuel/app/classes/Controller/Welcome.php
+++ fuel/app/classes/Controller/Welcome.php
@@ -11,4 +11,11 @@ class Welcome extends Classes\Controller\Base
 		$view->body = '<p><strong>TEST!</strong></p>';
 		return $view;
 	}
+
+	public function action_catchall()
+	{
+		$view = $this->app->forge('View', 'welcome');
+		$view->body = '<p><strong>Gevangen: </strong>'.implode('/', func_get_args()).'</p>';
+		return $view;
+	}
 }
